46835f833835bcec690e7e2bc092659754bde259
Elliot <hack00mind@gmail.com>
2024-01-16 20:00:59 +0800

    chore: bump lifecycle to 2.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,11 +62,12 @@ cargo {
     targetDirectory = "./src/main/rust/liushu/target"
 }
 
-
 dependencies {
+    val lifecycleVersion = "2.7.0"
+
     implementation("net.java.dev.jna:jna:5.9.0@aar")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
@@ -74,9 +75,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-service:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-service:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk-android:1.13.7")
     testImplementation("io.mockk:mockk-agent:1.13.7")
