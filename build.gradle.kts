35ed4452ec96c67899aea4adefc28a131eeccb4f
Elliot <hack00mind@gmail.com>
2024-01-16 19:52:22 +0800

    chore: bump gradle to 8.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3"
 }
