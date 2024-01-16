1fa08eadb24cd1fe7b895bbb052ddfd777b122ac
Elliot <hack00mind@gmail.com>
2024-01-16 17:27:09 +0800

    chore: migrate build logic from groovy to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.mozilla.rust-android-gradle:plugin:0.9.3")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3"
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Liushu"
-include ':app'
+include(":app")
